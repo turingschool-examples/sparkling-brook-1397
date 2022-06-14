8be14593624f1681c18bb2dee1e3a618d0f635ab
Sage Skaff <skaffsage@gmail.com>
2022-06-14 09:52:46 -0700

    Add name and hometown of competitions teams to show page

--- spec/features/competitions/show_spec.rb
+++ spec/features/competitions/show_spec.rb
@@ -23,6 +23,25 @@ RSpec.describe 'Competition Show Page' do
   end
 
   it 'lists teams in this competition and their attributes' do
+    comp1 = Competition.create(name: 'The Sports Thing', location: 'Somewhere', sport: 'Sportsball')
+    comp2 = Competition.create(name: 'Futbol Championship', location: 'Elsewhere', sport: 'Futbol')
+
+    team1 = Team.create(nickname: 'Orbs', hometown: 'Earth')
+    team2 = Team.create(nickname: 'Cubes', hometown: 'Mars')
+    team3 = Team.create(nickname: 'Hedrons', hometown: 'Venus')
+
+    CompetitionTeam.create(team: team1, competition: comp1)
+    CompetitionTeam.create(team: team2, competition: comp1)
+    CompetitionTeam.create(team: team3, competition: comp2)
+
+    visit competition_path(comp1.id)
+
+    expect(page).to have_content('Orbs')
+    expect(page).to have_content('Earth')
+    expect(page).to have_content('Cubes')
+    expect(page).to have_content('Mars')
+    expect(page).to_not have_content('Hedrons')
+    expect(page).to_not have_content('Venus')
   end
 
   it 'shows average age of all players in the competition' do
