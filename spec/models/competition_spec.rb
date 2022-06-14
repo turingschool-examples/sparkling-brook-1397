04c25005f5adb491d81f26aff3c665cb19943ee6
Sage Skaff <skaffsage@gmail.com>
2022-06-14 08:37:09 -0700

    Add relationships

---
+++ spec/models/competition_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe Competition, type: :model do
+  it { should have_many :competition_teams }
+  it { should have_many(:teams).through(:competition_teams) }
+end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Team, type: :model do
   it { should have_many :players }
+  it { should have_many :competition_teams }
+  it { should have_many(:competitions).through(:competition_teams) }
 end
+
