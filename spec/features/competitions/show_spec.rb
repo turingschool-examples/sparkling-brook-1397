23c25800db94f076ac400895dcf726dc366b09b3
Sage Skaff <skaffsage@gmail.com>
2022-06-14 10:07:32 -0700

    Add average age of all players method

--- spec/features/competitions/show_spec.rb
+++ spec/features/competitions/show_spec.rb
@@ -45,5 +45,24 @@ RSpec.describe 'Competition Show Page' do
   end
 
   it 'shows average age of all players in the competition' do
+    comp1 = Competition.create(name: 'The Sports Thing', location: 'Somewhere', sport: 'Sportsball')
+    comp2 = Competition.create(name: 'Futbol Championship', location: 'Elsewhere', sport: 'Futbol')
+
+    team1 = Team.create(nickname: 'Orbs', hometown: 'Earth')
+    team2 = Team.create(nickname: 'Cubes', hometown: 'Mars')
+    team3 = Team.create(nickname: 'Hedrons', hometown: 'Venus')
+
+    player1 = team1.players.create(name: 'Aeb', age: 277)
+    player2 = team1.players.create(name: 'Beeb', age: 138)
+    player3 = team2.players.create(name: 'Cen', age: 90)
+    player4 = team2.players.create(name: 'Dyth', age: 83)
+    player5 = team3.players.create(name: 'Eno', age: 122)
+
+    CompetitionTeam.create(team: team1, competition: comp1)
+    CompetitionTeam.create(team: team2, competition: comp1)
+    CompetitionTeam.create(team: team3, competition: comp2)
+
+    visit competition_path(comp1.id)
+    expect(page).to have_content('Average Player Age: 142')
   end
 end
--- spec/models/competition_spec.rb
+++ spec/models/competition_spec.rb
@@ -3,4 +3,21 @@ require 'rails_helper'
 RSpec.describe Competition, type: :model do
   it { should have_many :competition_teams }
   it { should have_many(:teams).through(:competition_teams) }
+
+  it 'can calculate the average age of all players' do
+    comp1 = Competition.create(name: 'The Sports Thing', location: 'Somewhere', sport: 'Sportsball')
+
+    team1 = Team.create(nickname: 'Orbs', hometown: 'Earth')
+    team2 = Team.create(nickname: 'Cubes', hometown: 'Mars')
+
+    player1 = team1.players.create(name: 'Aeb', age: 277)
+    player2 = team1.players.create(name: 'Beeb', age: 138)
+    player3 = team2.players.create(name: 'Cen', age: 90)
+    player4 = team2.players.create(name: 'Dyth', age: 83)
+
+    CompetitionTeam.create(team: team1, competition: comp1)
+    CompetitionTeam.create(team: team2, competition: comp1)
+
+    expect(comp1.average_age_of_all_players).to eq(147)
+  end
 end
